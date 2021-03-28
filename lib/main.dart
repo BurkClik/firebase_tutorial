d34b6793ce267691bb3e9ba17ef62bcb764a30d3
BurkClik <burk.clik@gmail.com>
2021-03-28 23:02:32 +0300

    Firebase Entegration

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,11 @@ import 'package:firebase_demo/screens/welcome.dart';
 import 'package:firebase_demo/theme/theme.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(MyApp());
 }
 
